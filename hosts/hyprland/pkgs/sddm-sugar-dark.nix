2ea5792db7289837a41a80bd3a64636fde4a6079
Filipe S0usa <pipaosilva18@gmail.com>
2024-04-26 13:40:59 +0100

    Fixed wrong spealing of GitHub

--- hosts/hyprland/pkgs/sddm-sugar-dark.nix
+++ hosts/hyprland/pkgs/sddm-sugar-dark.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation {
   name = "sddm-sugar-dark";
-  src = pkgs.fetchFromGithub {
+  src = pkgs.fetchFromGitHub {
     owner = "MarianArlt";
     repo = "sddm-sugar-dark";
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
