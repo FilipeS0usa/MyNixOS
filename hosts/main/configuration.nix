532e6f8ff1ac0c759b2413e671509abf33963d8d
Filipe S0usa <pipaosilva18@gmail.com>
2024-05-03 18:40:18 +0100

    Added new host

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -117,6 +117,7 @@
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # neovim
     wget
+    vscode
     # Personal
     neofetch
     git
@@ -181,5 +182,6 @@
   ''
   ${secrets.gitlabserver}
   ${secrets.passboltserver}
+  ${secrets.vmwareserver}
   '';
 }
