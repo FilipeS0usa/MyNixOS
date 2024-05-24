214c90e4315c2a77ed4ec522e5030975f86d8ba8
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-05-24 15:25:02 +0100

    Update gtk theme

--- homeManagerModules/hyprland/gtk-qt.nix
+++ homeManagerModules/hyprland/gtk-qt.nix
@@ -15,18 +15,18 @@
     gtk = {
       enable = true;
       theme = {
-        package = pkgs.adw-gtk3;
-        name = "adw-gtk3";
+        package = pkgs.orchis-theme;
+        name = "orchis-theme";
       };
       cursorTheme = {
         package = pkgs.bibata-cursors;
         name = "Bibata-Modern-Classic";
       };
       iconTheme = {
-        package = pkgs.nerdfonts;
-        name = "FiraCode";
-        #package = pkgs.papirus-icon-theme;
-        #name = "Papirus-Dark";
+        # package = pkgs.nerdfonts;
+        # name = "FiraCode";
+        package = pkgs.papirus-icon-theme;
+        name = "Papirus-Dark";
       };
       font = {
         package = pkgs.nerdfonts;
