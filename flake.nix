2ab3448bc5bf4096a764e77b96cd7ddf237abf15
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-07-21 21:21:14 +0100

    Add some new repos to flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,11 @@
     hyprland.url = "github:hyprwm/Hyprland";
 
     stylix.url = "github:danth/stylix";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, self, ... }@inputs:
