4ef22676d6f8795a6b2ed117059738c9ba9e877b
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-07-23 19:30:32 +0100

    Update h-m default.nix commented hyprland and add stylix and nvim

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -8,20 +8,21 @@
     ./cliPrograms/bash.nix
     ./cliPrograms/files/files.nix # Place home files of Cli Programs
     ./cliPrograms/nvim.nix 
-    # ./cliPrograms/experimentation/nixvim.nix
+    ./cliPrograms/nixvim.nix
 
     # Programs
     ./programs/firefox.nix
+    ./programs/stylix.nix
 
     # Hyprland
-    ./hyprland/gtk-qt.nix
-    #./hyprland/hyprland.nix
-    ./hyprland/swaylock.nix
-    ./hyprland/waybar.nix
-    ./hyprland/wofi.nix
-    ./hyprland/network-manager-applet.nix
-    ./hyprland/dunst.nix
-    ./hyprland/files/files.nix # Place home files of Desktop Environment
+    # ./hyprland/gtk-qt.nix
+    # #./hyprland/hyprland.nix
+    # ./hyprland/swaylock.nix
+    # ./hyprland/waybar.nix
+    # ./hyprland/wofi.nix
+    # ./hyprland/network-manager-applet.nix
+    # ./hyprland/dunst.nix
+    # ./hyprland/files/files.nix # Place home files of Desktop Environment
   ];
 }
 
