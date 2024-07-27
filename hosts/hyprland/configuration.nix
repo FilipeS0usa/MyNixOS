68f4aa14b64e02ba3c121a59eb5da52229ed38dd
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-07-27 02:37:59 +0100

    Update hyprland/configuration to accept modules

--- hosts/hyprland/configuration.nix
+++ hosts/hyprland/configuration.nix
@@ -1,72 +1,33 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, inputs, ... }:
+{  pkgs, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./../../nixosModules/default.nix
+      ./../../modules/nixos
     ];
 
+  # Programs
+  qemu-kvm.enable = true;
+  docker.enable = true;
+  virtualbox.enable = false;
 
-  # Enables programs
-  vm.enable = true;
-
-  # Enables hyprland
-  programs.hyprland = {
-    enable = true;
-	package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-  };
-
-  # Enables services
+  # General System Configs
+  ## Bootloader
+  bootloader.enable = true;
+  ## Network
+  network.enable = true;
+  ## Timezone/time
+  time-location.enable = true; 
+  ## Desktop Environment
+  hyprland-de.enable = true; 
+  ## Sound
   pipewire.enable = true;
-  fonts.enable = true;
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "b3rryOS-Nix"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Lisbon";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_PT.UTF-8";
-    LC_IDENTIFICATION = "pt_PT.UTF-8";
-    LC_MEASUREMENT = "pt_PT.UTF-8";
-    LC_MONETARY = "pt_PT.UTF-8";
-    LC_NAME = "pt_PT.UTF-8";
-    LC_NUMERIC = "pt_PT.UTF-8";
-    LC_PAPER = "pt_PT.UTF-8";
-    LC_TELEPHONE = "pt_PT.UTF-8";
-    LC_TIME = "pt_PT.UTF-8";
-  };
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    #NIXOS_OZONE_WL = "1";
-  };
-
-  hardware = {
-    opengl.enable = true;
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  ## FireWall
+  firewall.enable = true; 
+  ## Extra NixOS Options (ex.: Flake, Allow unfree, ...)
+  extras.enable = true;
+  ## Optimizations 
+  storage-optimization.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.b3rrypi = {
@@ -74,16 +35,6 @@
     description = "Filipe Silva";
     extraGroups = [ "networkmanager" "wheel" ];
   };
-
-  # Enable experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0"
-  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -100,20 +51,17 @@
     # Note taking
     obsidian
     # Cli Tools
-    neofetch
     wget
+    git
     git-crypt
     # Tools
     mongodb-compass
     winbox
     keepassxc
-    # Hyprland Extras
-    hyprpaper
-    hyprpicker
-    hypridle
-    hyprlock
-    hyprcursor
   ];
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -123,17 +71,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -141,14 +78,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
-  # Enabling docker
-  virtualisation.docker.enable = true;
-  #virtualisation.docker.extraOptions = "--dns 8.8.8.8 --dns 8.8.4.4";
-  #virtualisation.docker.rootless = {
-  #enable = true;
-  #setSocketVariable = true;
-  #};
-  users.extraGroups.docker.members = [ "b3rrypi" ];
-  #users.extraGroups.b3rrypi.members = [ "b3rrypi" ];
 }
