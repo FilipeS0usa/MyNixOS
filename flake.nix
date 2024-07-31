e92c7162599bbcdf4e6f3437a741e2c39beca7ad
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-07-31 11:56:33 +0100

    Update flake to correct hyprland url

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     # Hyprland
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     # Stylix for styling my OS collors
     stylix.url = "github:danth/stylix";
