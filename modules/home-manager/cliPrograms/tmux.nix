705628574dc235e6e5364891fa0e58935fc73804
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-08-03 22:40:37 +0100

    Update tmux configuration

--- modules/home-manager/cliPrograms/tmux.nix
+++ modules/home-manager/cliPrograms/tmux.nix
@@ -5,8 +5,35 @@
   };
 
   config = lib.mkIf config.tmux.enable {
-    programs.tmux= {
+    programs.tmux = {
       enable = true;
+      # Window index start at 1
+      baseIndex = 1; 
+      # Change prefix
+      prefix = "C-Space";
+      # Enable mouse selection
+      mouse = true;
+      # Add plugins
+      plugins = with pkgs; [
+        tmuxPlugins.vim-tmux-navigator
+        {
+          plugin = tmuxPlugins.yank;
+          extraConfig = ''
+            # set vi-mode
+            set-window-option -g mode-keys vi
+            # keybindings
+            bind-key -T copy-mode-vi v send-keys -X begin-selection
+            bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+            bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel 
+          '';
+        }
+        tmuxPlugins.sensible
+      ];
+      extraConfig = ''
+        # Open panes in current directory
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+      '';
     };
   };
 }
