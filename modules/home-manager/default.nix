5f02d33b0484528a1143f69248ae1e306e4cbdf0
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-08-03 23:02:56 +0100

    Removed files from home-manager/default.nix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -6,7 +6,6 @@
     ./cliPrograms/kitty.nix 
     ./cliPrograms/starship.nix
     ./cliPrograms/bash.nix
-    ./cliPrograms/files/files.nix # Place home files of Cli Programs
     ./cliPrograms/nixvim.nix
     ./cliPrograms/zathura.nix
     ./cliPrograms/tmux.nix
