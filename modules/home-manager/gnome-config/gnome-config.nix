776eba93a3dd7dd13d6e0494eaf307796c2e59b8
FilipeS0usa <77252969+FilipeS0usa@users.noreply.github.com>
2024-08-05 01:42:15 +0100

    Add new keybind to gnome-config

--- modules/home-manager/gnome-config/gnome-config.nix
+++ modules/home-manager/gnome-config/gnome-config.nix
@@ -20,6 +20,7 @@
       # Keybinds to window management
       "org/gnome/desktop/wm/keybindings" = {
         close = ["<Super>w"];
+        minimize = "disabled";
         switch-applications = "disabled";
         move-to-workspace-1 = ["<Shift><Super>1"];
         move-to-workspace-2 = ["<Shift><Super>2"];
