d875954ee54769ce59d1cb9569577121e3ccce34
FilipeS0usa <filipes0usa.git@proton.me>
2024-09-09 09:26:32 +0100

    Update number of workspaces in gnome to 8

--- modules/home-manager/gnome-config/gnome-config.nix
+++ modules/home-manager/gnome-config/gnome-config.nix
@@ -27,10 +27,18 @@
         move-to-workspace-2 = ["<Shift><Super>2"];
         move-to-workspace-3 = ["<Shift><Super>3"];
         move-to-workspace-4 = ["<Shift><Super>4"];
+        move-to-workspace-5 = ["<Shift><Super>5"];
+        move-to-workspace-6 = ["<Shift><Super>6"];
+        move-to-workspace-7 = ["<Shift><Super>7"];
+        move-to-workspace-8 = ["<Shift><Super>8"];
         switch-to-workspace-1 = ["<Super>1"];
         switch-to-workspace-2 = ["<Super>2"];
         switch-to-workspace-3 = ["<Super>3"];
         switch-to-workspace-4 = ["<Super>4"];
+        switch-to-workspace-5 = ["<Super>5"];
+        switch-to-workspace-6 = ["<Super>6"];
+        switch-to-workspace-7 = ["<Super>7"];
+        switch-to-workspace-8 = ["<Super>8"];
         cycle-windows = ["<Super>Tab"];
       };
       # Call Custom keybinds and add another ones
@@ -47,6 +55,9 @@
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         ];
+      };
+      "org/gnome/desktop/wm/preferences" = { 
+        num-workspaces = 8;
       };
       # Custom Keybind to open kitty with tmux
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
